f43f123723cd13848ae41b4883323c6d9f90cc81
yutainoue <yutainoue009@gmail.com>
2014-08-05 15:31:47 +0900

    初gem

---
+++ lib/hoge.rb
@@ -0,0 +1,12 @@
+require "hoge/version"
+
+module Hoge
+  # Your code goes here...
+  def self.hello
+    'hello gem!'
+  end
+
+  def hello2
+    'hello gem! 2'
+  end
+end
---
+++ lib/hoge/version.rb
@@ -0,0 +1,3 @@
+module Hoge
+  VERSION = "1.0.0"
+end
